cc2218558cd7a1e74a3b8168d13325436497fd66
05Ambuj <78725990+05Ambuj@users.noreply.github.com>
2024-09-02 17:59:46 +0530

    Update WebConfig.java

--- src/main/java/com/project/bloggiator/config/WebConfig.java
+++ src/main/java/com/project/bloggiator/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5500","http://127.0.0" +
-                        ".1:5500")
+                        ".1:5500","https://bloggiator-backend-production.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
